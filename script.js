d1ab2b73d0c3124925db5aba0c2cf8d9bb604501
MichDelReal <159098344+michdelrealp@users.noreply.github.com>
2024-04-10 02:19:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ document.getElementById('creditDebit').addEventListener('click', () => {
     // Add your credit/debit card payment processing code here
     alert('Credit/Debit Card selected');
 });
+const processBtn = document.querySelector('.process-btn');
+
+processBtn.addEventListener('click', () => {
+  // Aquí puedes agregar el código JavaScript que quieres ejecutar al hacer clic en el botón
+  console.log('Botón "Procesar" presionado');
+});
